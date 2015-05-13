b20255f62be33852450871e810c563178bd6cb0c
Krenyez <Krenyez@Krenyez>
2015-05-14 06:41:37 +0700

    Tugas besar

---
+++ src/KenyamananRuangKelas.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Krenyez
+ */
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+public class KenyamananRuangKelas {
+	private String Kebisingan;
+	private String Bau;
+	private String Kebocoran;
+	private String Kerusakan;
+	private String Keausan;
+	
+	void setKebisingan(String Kebisingan){
+        this.Kebisingan = Kebisingan;
+    }
+    
+    String getKebisingan(){
+        return this.Kebisingan;
+    }
+    void setBau(String Bau){
+        this.Bau = Bau;
+    }
+    
+    String getBau(){
+        return this.Bau;
+    }
+    void setKebocoran(String Kebocoran){
+        this.Kebocoran = Kebocoran;
+    }
+    
+    String getKebocoran(){
+        return this.Kebocoran;
+    }
+    void setKerusakan(String Kerusakan){
+        this.Kerusakan = Kerusakan;
+    }
+    
+    String getKerusakan(){
+        return this.Kerusakan;
+    }
+    void setKeausan(String Keausan){
+        this.Keausan = Keausan;
+    }
+    
+    String getKeausan(){
+        return this.Keausan;
+    }	
+}
